b3a638042e4e1bf7957c5f33082e6a57953dcc83
olek1305 <olek1305@gmail.com>
2023-06-24 07:08:48 +0200

    fix bug

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -34,6 +34,7 @@ class Product extends Model
         foreach ($products as $product) {
             $total = $total + ($product->getPrice() * $productsInSession[$product->getId()]);
         }
+        return $total;
     }
 
     public function getId()
